2b7916677660ffc67ae4135f20ee415a55afe79e
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-08 22:07:15 +0700

    Update week4.cpp

---
+++ week4/week4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// đếm số lượng ước của n
+int dem_uoc(int n);
+
+int main()
+{
+    int n;
+   
+     do {
+        cout << "Nhap so n: "; cin >> n;
+        if (n<=0) cout <<"Vui long nhap so duong" << endl;
+    } while (n<=0);
+
+    cout << "so luong uoc cua n: " << dem_uoc(n) << endl;
+    return 0;
+    
+}
+
+int dem_uoc(int n)
+{
+    int dem=0;
+    for (int i=1;i<=n;i++)
+    {
+        if(n%i==0) dem++;
+    }
+
+    return dem;
+}
