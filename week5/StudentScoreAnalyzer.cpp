021851a88922b8db07fb2680b9f3a3bcb1c7f765
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-09 10:35:15 +0700

    Update StudentScoreAnalyzer.cpp

--- week5/StudentScoreAnalyzer.cpp
+++ week5/StudentScoreAnalyzer.cpp
@@ -128,3 +128,34 @@ int main()
     // =====================================
     // 4. SECOND HIGHEST SCORE
     // =====================================
+     double highest = scores[0];
+    double secondHighest = -1;
+
+    for (int i = 1; i < N; i++) {
+
+        if (scores[i] > highest) {
+
+            secondHighest = highest;
+            highest = scores[i];
+        }
+        else if (scores[i] > secondHighest &&
+                 scores[i] < highest) {
+
+            secondHighest = scores[i];
+        }
+    }
+
+    cout << "\n===== 4. SECOND HIGHEST =====\n";
+
+    cout << "Highest score: "
+         << highest
+         << endl;
+
+    cout << "Second highest score: "
+         << secondHighest
+         << endl;
+
+
+    // =====================================
+    // 5. SEARCH FOR A SCORE
+    // =====================================
