d534ec86db1025d423cc5e1d87df05f3e0631037
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-09 09:38:20 +0700

    Update StudentScoreAnalyzer.cpp

--- week5/StudentScoreAnalyzer.cpp
+++ week5/StudentScoreAnalyzer.cpp
@@ -54,3 +54,22 @@ for (int i = 0; i < N; i++) {
         cout << "ROT\n";
     }
 }
+int passedCount = 0;
+int failedCount = 0;
+
+for (int i = 0; i < N; i++) {
+
+    if (scores[i] >= 5.0) {
+        passedCount++;
+    }
+    else {
+        failedCount++;
+    }
+}
+
+double passRate = ((double)passedCount / N) * 100;
+
+cout << "\n===== THONG KE =====\n";
+cout << "So hoc sinh Dau : " << passedCount << "\n";
+cout << "So hoc sinh Rot : " << failedCount << "\n";
+cout << "Ty le dau       : " << passRate << "%\n";
