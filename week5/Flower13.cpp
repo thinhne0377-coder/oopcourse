3bf97c7d41dce005ac70bba177da70e9ea8b7503
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-09 22:03:10 +0700

    Update Flower13.cpp

--- week5/Flower13.cpp
+++ week5/Flower13.cpp
@@ -10,3 +10,11 @@ struct Flower {
 };
 Flower flowers[20];
 int n;
+do {
+    cout << "How many flowers? ";
+    cin >> n;
+
+    if (n < 1 || n > 20) {
+        cout << "Invalid! Enter from 1 to 20.\n";
+    }
+} while (n < 1 || n > 20);
