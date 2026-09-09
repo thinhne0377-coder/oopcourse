09a21b82ee56e66b8db8d77e6b82d972c8407298
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-09 09:38:01 +0700

    Update StudentScoreAnalyzer.cpp

--- week5/StudentScoreAnalyzer.cpp
+++ week5/StudentScoreAnalyzer.cpp
@@ -40,3 +40,17 @@ for (int i = 0; i < N; i++) {
     cout << "Hoc sinh " << i + 1
          << ": " << scores[i] << "\n";
 }
+cout << "\n===== KET QUA =====\n";
+
+for (int i = 0; i < N; i++) {
+
+    cout << "Hoc sinh " << i + 1
+         << ": " << scores[i] << " -> ";
+
+    if (scores[i] >= 5.0) {
+        cout << "DAU\n";
+    }
+    else {
+        cout << "ROT\n";
+    }
+}
