fc708a9a52edb4f1abbabd905a2f6d92d13fc702
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 10:34:23 +0700

    Update step4.cpp

--- week6/step4.cpp
+++ week6/step4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+
+// Ham tim so lon hon
 int maxValue(int a, int b)
 {
     if (a > b)
@@ -11,15 +13,25 @@ int maxValue(int a, int b)
         return b;
     }
 }
+
 int main()
 {
+    // Khai bao 2 bien
+    int a, b;
 
+    // Nhap a
+    cout << "Nhap so a: ";
+    cin >> a;
+
+    // Nhap b
+    cout << "Nhap so b: ";
+    cin >> b;
+
+    // Goi ham maxValue va luu ket qua
+    int max = maxValue(a, b);
+
+    // In ket qua
+    cout << "So lon hon la: " << max << endl;
+
+    return 0;
 }
-int a, b;
-cout << "Nhap so a: ";
-cin >> a;
-
-cout << "Nhap so b: ";
-cin >> b;
-int max = maxValue(a, b);
-cout << "So lon hon la: " << max << endl;
