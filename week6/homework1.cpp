a193adc77c0e52ed7fa63adfaea85ad659d47bed
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 11:07:51 +0700

    Create homework1.cpp

---
+++ week6/homework1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// =====================================================
+// FUNCTION PROTOTYPES
+// =====================================================
+
+void inputStudents(int &N, double scores[]);
+void countExcellentStudents(int N, double scores[]);
+void countStudentsInRange(int N, double scores[]);
+void findStudentsAboveAverage(int N, double scores[]);
+void findSecondHighestScore(int N, double scores[]);
+void searchForScore(int N, double scores[]);
+void countScoreFrequency(int N, double scores[]);
+void findFirstFailingStudent(int N, double scores[]);
+void compareAdjacentStudents(int N, double scores[]);
+void findLongestPassStreak(int N, double scores[]);
+void createScoreDistribution(int N, double scores[]);
