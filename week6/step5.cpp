80bc0f8e957a17ca9c4172ff0a90074607332b8b
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 10:37:34 +0700

    Update step5.cpp

--- week6/step5.cpp
+++ week6/step5.cpp
@@ -39,3 +39,12 @@ ScoreInfo analyzeScores(const vector<double>& scores) {
 }
 int main() {
     vector<double> scores = {8.5, 7.0, 9.5, 6.0, 8.0};
+    ScoreInfo info = analyzeScores(scores);
+
+    cout << "Total: " << info.sum << endl;
+    cout << "Average: " << info.average << endl;
+    cout << "Max score: " << info.maxScore << endl;
+    cout << "Min score: " << info.minScore << endl;
+
+    return 0;
+}
