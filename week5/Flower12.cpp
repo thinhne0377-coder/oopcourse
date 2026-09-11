2a419b549551ce3ed973b18437d056a59f08f1e8
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 08:34:06 +0700

    Create Flower12.cpp

---
+++ week5/Flower12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    const int MAX = 20;
+
+    string name[MAX];
+    double price[MAX];
+    int quantity[MAX];
+    string type[MAX];
+
+    int n;
+
+    // =========================
+    // INPUT NUMBER OF FLOWERS
+    // =========================
