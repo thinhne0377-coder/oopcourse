99467b48679e14b959aaed243c5d95a73de6576b
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 10:36:30 +0700

    Update step5.cpp

--- week6/step5.cpp
+++ week6/step5.cpp
@@ -8,3 +8,15 @@ struct ScoreInfo {
     double maxScore;
     double minScore;
 };
+ScoreInfo analyzeScores(const vector<double>& scores) {
+    ScoreInfo result;
+
+    result.sum = 0;
+    result.count = scores.size();
+
+    if (result.count == 0) {
+        result.average = 0;
+        result.maxScore = 0;
+        result.minScore = 0;
+        return result;
+    }
