1ecc78dabf8715f156745fd9cbbf45122e3b0f1f
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 10:49:53 +0700

    Update step1,2,3,4,5.cpp

--- week6/step1,2,3,4,5.cpp
+++ week6/step1,2,3,4,5.cpp
@@ -44,3 +44,52 @@ double calculateAverage(double score1, double score2) {
 
     return average;
 }
+// ==================================================
+// STEP 5: Function returns multiple results using struct
+// ==================================================
+
+struct ScoreInfo {
+    double sum;
+    double average;
+    int count;
+    double maxScore;
+    double minScore;
+};
+
+ScoreInfo analyzeScores(const vector<double>& scores) {
+    ScoreInfo result;
+
+    result.sum = 0;
+    result.count = scores.size();
+
+    // Nếu danh sách rỗng
+    if (result.count == 0) {
+        result.average = 0;
+        result.maxScore = 0;
+        result.minScore = 0;
+
+        return result;
+    }
+
+    // Gán giá trị ban đầu
+    result.maxScore = scores[0];
+    result.minScore = scores[0];
+
+    // Duyệt danh sách điểm
+    for (double s : scores) {
+        result.sum += s;
+
+        if (s > result.maxScore) {
+            result.maxScore = s;
+        }
+
+        if (s < result.minScore) {
+            result.minScore = s;
+        }
+    }
+
+    // Tính điểm trung bình
+    result.average = result.sum / result.count;
+
+    return result;
+}
