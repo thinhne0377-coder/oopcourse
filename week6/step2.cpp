c0a8135358d2f6d25793bc38e94ff89796c96f41
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 10:14:55 +0700

    Update step2.cpp

--- week6/step2.cpp
+++ week6/step2.cpp
@@ -1,19 +1,12 @@
-void showStudentID(int id)
-{
-    // câu lệnh
-}
 #include <iostream>
 
 using namespace std;
+
 void showStudentID(int id)
 {
     cout << "Student ID: " << id << endl;
 }
-int main()
-{
-    
-    return 0;
-}
+
 int main()
 {
     int id;
