4df33c4ff46a9cf853f1df9975a01b356c081946
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-11 11:08:41 +0700

    Update homework1.cpp

--- week6/homework1.cpp
+++ week6/homework1.cpp
@@ -82,3 +82,41 @@ int main()
 
     return 0;
 }
+// =====================================================
+// INPUT STUDENTS
+// =====================================================
+
+void inputStudents(int &N, double scores[])
+{
+    do
+    {
+        cout << "Nhap so luong hoc sinh: ";
+        cin >> N;
+
+        if (N < 2 || N > 20)
+        {
+            cout << "So luong khong hop le! "
+                 << "Vui long nhap tu 2 den 20.\n";
+        }
+
+    } while (N < 2 || N > 20);
+
+
+    for (int i = 0; i < N; i++)
+    {
+        do
+        {
+            cout << "Nhap diem cho hoc sinh "
+                 << i + 1 << ": ";
+
+            cin >> scores[i];
+
+            if (scores[i] < 0 || scores[i] > 10)
+            {
+                cout << "Diem khong hop le! "
+                     << "Vui long nhap tu 0 den 10.\n";
+            }
+
+        } while (scores[i] < 0 || scores[i] > 10);
+    }
+}
