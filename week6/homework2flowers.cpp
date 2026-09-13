d817754da7a82a91728f1c56700c662e9b2744de
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-13 10:47:13 +0700

    Update homework2flowers.cpp

--- week6/homework2flowers.cpp
+++ week6/homework2flowers.cpp
@@ -15,3 +15,23 @@ typedef struct {
   char type[20];
 
 } Flower;
+void nhapflowers(Flower DS[], int N) {
+  printf("====== Nhap thong tin hoa ======\n");
+
+  for (int i = 0; i < N; i++) {
+    DS[i].no = i + 1;
+    cout << "Nhap thong tin hoa " << i + 1 << ": " << endl;
+    cout << "Name: ";
+    cin.getline(DS[i].name, 20);
+    cout << "Price: ";
+    cin >> DS[i].price;
+    cin.ignore();
+    cout << "Quantity: ";
+    cin >> DS[i].quantity;
+    cin.ignore();
+    cout << "Type: ";
+    cin.getline(DS[i].type, 20);
+    cout << endl;
+  }
+  cout << endl << endl;
+}
