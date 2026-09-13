628baccc9ea974a7c50c426d052d2b0d962b1118
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-13 10:47:49 +0700

    Update homework2flowers.cpp

--- week6/homework2flowers.cpp
+++ week6/homework2flowers.cpp
@@ -46,3 +46,23 @@ void inflowers(Flower DS[], int N) {
   }
   cout << endl << endl;
 }
+// câu 2: find the most expensive flowers
+void max_price(Flower DS[], int N) {
+  float max = DS[0].price;
+
+  for (int i = 1; i < N; i++) {
+    if (DS[i].price > max) {
+      max = DS[i].price;
+    }
+  }
+
+  printf("====== HOA CO GIA CAO NHAT ======\n");
+  printf("%-12s|%-32s|%4s|%10s|%20s\n", "No", "Name", "Price", "Qty", "Type");
+
+  for (int i = 0; i < N; i++) {
+    if (DS[i].price == max) {
+      printf("%-12d|%-32s|%4.1f|%10d|%20s\n", DS[i].no, DS[i].name, DS[i].price,
+             DS[i].quantity, DS[i].type);
+    }
+  }
+}
