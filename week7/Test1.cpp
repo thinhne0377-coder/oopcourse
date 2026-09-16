0bb58fb5f4710b526ff7b4f84914dc6fb8e5e7ec
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-16 09:12:35 +0700

    Update Test1.cpp

--- week7/Test1.cpp
+++ week7/Test1.cpp
@@ -16,3 +16,17 @@ struct Computer
     string pwd;
     string model;
     int year;
+ // Ham print
+    void print()
+    {
+        cout << "==============================" << endl;
+        cout << "ID       : " << id << endl;
+        cout << "Ten may  : " << tenMay << endl;
+        cout << "HDH      : " << hdh << endl;
+        cout << "Username : " << username << endl;
+        cout << "Password : " << pwd << endl;
+        cout << "Model    : " << model << endl;
+        cout << "Year     : " << year << endl;
+        cout << "==============================" << endl;
+    }
+};
