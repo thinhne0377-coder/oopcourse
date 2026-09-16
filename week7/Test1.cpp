3cc3045770d638a3df75524f913158e3b3a5dc0e
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-16 09:06:31 +0700

    Create Test1.cpp

---
+++ week7/Test1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// ===============================
+// KHAI BAO STRUCT COMPUTER
+// ===============================
+struct Computer
+{
+    int id;
+    string tenMay;
+    string hdh;
+    string username;
+    string pwd;
+    string model;
+    int year;
+};
