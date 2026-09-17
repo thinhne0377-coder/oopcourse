f68c3de677d7c6f5e8e9704e27b082f8f2f20874
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-17 21:20:25 +0700

    Update homeworkbookandlibrary.cpp

--- week7/homeworkbookandlibrary.cpp
+++ week7/homeworkbookandlibrary.cpp
@@ -1,94 +1,139 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-struct Book {
-    int id;
-    string name;
+#define MAX 5
+
+struct Book{
+    int idBook;
+    string nameBook;
     string author;
 
-    void inputInfo() {
-        cin >> name;
-        cin >> author;
-        cin >> id;
-    }
+    void inputInfo(){
+        cout << "Enter the id of the book: ";
+        cin >> idBook;
 
-    void outputInfo() {
-        cout << name;
-        cout << id;
-        cout << author;
+        cin.ignore();
+
+        cout << "Enter the name of the book: ";
+        getline (cin, nameBook);
+
+        cout << "Enter the author of the book : ";
+        getline (cin, author);
+
+        cout << endl;
+    } 
+
+    void outputInfo(){
+        cout << "========================" << endl;
+        cout << "   |   ID      :" << idBook << endl;
+        cout << "   |   Name    :" << nameBook << endl;
+        cout << "   |   Author  :" << author << endl;
+        cout << "========================" << endl;
+
     }
 };
 
-struct Library {
+struct Library{
     int idLibrary;
     string nameLibrary;
-    int numBooks;
-    Book books[100];
+    int bookNumber;
 
-    void inputLibrary() {
+    Book bookList[MAX];
+
+    void inputLibrary(){
+        cout << "*************************" << endl;
+        cout << "Enter the id of the library: ";
         cin >> idLibrary;
-        cin >> nameLibrary;
-        cin >> numBooks;
+        
+        cin.ignore();
 
-        for (int i = 0; i < numBooks; i++) {
-            cin >> books[i].id;
-            cin >> books[i].name;
-            cin >> books[i].author;
+        cout << "Enter the name of the library: ";
+        getline (cin, nameLibrary);
+
+        cout << "Enter the number of the book: ";
+        cin >> bookNumber;
+
+        for (int i= 0; i < bookNumber; i++){
+            bookList[i].inputInfo();
+        }
+        cout << "*************************" << endl;
+    }
+
+    void outputLibrary(){
+        cout << "<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+        cout << "ID of the library: " << idLibrary << endl;
+        cout << "Name of the library: " << nameLibrary << endl;
+        cout << "The number of books we currently have: " << bookNumber << endl;
+        for (int i = 0; i < bookNumber; i++){
+            bookList[i].outputInfo();
+        }
+        cout << "<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+    }
+
+    bool checkLibrary (){
+        if (idLibrary == 123 && nameLibrary == "A5-103"){
+            cout << "======================" << endl;
+            cout << "Welcome to the library" << endl;
+            cout << "======================" << endl;
+            return true;
+        }
+        else {
+            cout << "Sorry we don't have this library" << endl;
+            return false;
         }
     }
 
-    void outputLibrary() {
-        cout << idLibrary;
-        cout << nameLibrary;
-        cout << numBooks;
-
-        for (int i = 0; i < numBooks; i++) {
-            cout << books[i].id;
-            cout << books[i].name;
-            cout << books[i].author;
+    void addBook (Book s){
+        if (bookNumber < MAX){
+            bookList[bookNumber] = s;
+            bookNumber++;
+            cout << " --> Add book successfully!" << endl;
+        }
+        else {
+            cout << " --> Sorry there is no space left" << endl;
         }
     }
 
-    void addBook(Book a) {
-        books[numBooks] = a;
-        numBooks++;
-    }
-
-    void getBookInfo(int idBook) {
-        for (int i = 0; i < numBooks; i++) {
-            if (books[i].id == idBook) {
-                books[i].outputInfo();
+    // Get the book info directly when searching for ID
+    void getBookInfo(int idSearch){
+        for (int i = 0; i < bookNumber; i++){
+            if (bookList[i].idBook == idSearch){
+                cout << "===================" << endl;
+                cout << " ==> Found the book with the ID: " << idSearch << endl;
+                bookList[i].outputInfo();
+                return;
+            }
+            else {
+                cout << "Sorry there is no ID: " << idSearch << " match with the book" << endl;
             }
         }
     }
 
-    Book getBookInfo(int idBook) {
-        for (int i = 0; i < numBooks; i++) {
-            if (books[i].id == idBook) {
-                return books[i];
+    // Get the book info with the ID and return the whole struct
+    Book showBookInfo (int idSearch){
+        for (int i = 0; i < bookNumber; i++){
+            if (bookList[i].idBook == idSearch){
+                cout << "===================" << endl;
+                cout << " ==> Found the book with the ID: " << idSearch << endl;
+                bookList[i].outputInfo();
+                return bookList[i];
+            }
+            else {
+                cout << "Sorry there is no ID: " << idSearch << " match with the book" << endl;
             }
         }
-
-        // trong truong hop khong tim thay
-        Book book;
-        book.id = -1;
-        return book;
-    }
-
-    Book* getBooks() {
-        return books;
+        Book emptyBook;
+        emptyBook.idBook = -1;
+        emptyBook.nameBook == "N/A" ;
+        emptyBook.author == "N/A";
+        return emptyBook;
     }
 };
 
-int main() {
 
-    Library library;
-
-    library.inputLibrary();
-
-    library.outputLibrary();
-
+int main(){
+    cout << "Hello World" << endl;
+    
     return 0;
 }
