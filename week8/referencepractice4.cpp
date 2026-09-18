9569cc235e308aa815578ba77ac378175cc0bd09
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-18 10:01:13 +0700

    Update referencepractice4.cpp

--- week8/referencepractice4.cpp
+++ week8/referencepractice4.cpp
@@ -1,17 +1,94 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n = 7;
+// =============================
+// BAI 1
+// =============================
+void bai1() {
+    cout << "===== BAI 1 =====" << endl;
 
-    // r là reference của n
+    int n = 7;
     int& r = n;
 
-    // Thay đổi n thông qua reference r
     r = 15;
 
     cout << n << " " << r << endl;
+}
+
+// =============================
+// BAI 2
+// =============================
+void addFive(int& x) {
+    x += 5;
+}
+
+void bai2() {
+    cout << "===== BAI 2 =====" << endl;
+
+    int a = 10;
+
+    addFive(a);
+
+    cout << a << endl;
+}
+
+// =============================
+// BAI 3
+// =============================
+void swapValues(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+void bai3() {
+    cout << "===== BAI 3 =====" << endl;
+
+    int a = 4;
+    int b = 9;
+
+    swapValues(a, b);
+
+    cout << a << " " << b << endl;
+}
+
+// =============================
+// BAI 4
+// =============================
+void changeValue(int x) {
+    x = 100;
+}
+
+void changeRef(int& x) {
+    x = 200;
+}
+
+void bai4() {
+    cout << "===== BAI 4 =====" << endl;
+
+    int a = 5;
+
+    changeValue(a);
+    cout << "After changeValue: " << a << endl;
+
+    changeRef(a);
+    cout << "After changeRef: " << a << endl;
+}
+
+// =============================
+// MAIN
+// =============================
+int main() {
+    bai1();
+    cout << endl;
+
+    bai2();
+    cout << endl;
+
+    bai3();
+    cout << endl;
+
+    bai4();
 
     return 0;
 }
