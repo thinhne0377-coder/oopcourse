06187c303270cff3c95c10433547db249799d1ab
thinhne0377-coder <thinhne0377@gmail.com>
2026-09-18 10:11:23 +0700

    Update referencepractice4.cpp

--- week8/referencepractice4.cpp
+++ week8/referencepractice4.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// =============================
-// BAI 1
-// =============================
-void bai1() {
-    cout << "===== BAI 1 =====" << endl;
-
+// ======================================================
+// 1. MODIFY THROUGH A REFERENCE
+// ======================================================
+void practice1()
+{
     int n = 7;
     int& r = n;
 
@@ -15,16 +14,16 @@ void bai1() {
     cout << n << " " << r << endl;
 }
 
-// =============================
-// BAI 2
-// =============================
-void addFive(int& x) {
+// ======================================================
+// 2. USE A REFERENCE PARAMETER
+// ======================================================
+void addFive(int& x)
+{
     x += 5;
 }
 
-void bai2() {
-    cout << "===== BAI 2 =====" << endl;
-
+void practice2()
+{
     int a = 10;
 
     addFive(a);
@@ -32,40 +31,40 @@ void bai2() {
     cout << a << endl;
 }
 
-// =============================
-// BAI 3
-// =============================
-void swapValues(int& x, int& y) {
+// ======================================================
+// 3. SWAP USING REFERENCES
+// ======================================================
+void swapValues(int& x, int& y)
+{
     int temp = x;
     x = y;
     y = temp;
 }
 
-void bai3() {
-    cout << "===== BAI 3 =====" << endl;
-
-    int a = 4;
-    int b = 9;
+void practice3()
+{
+    int a = 4, b = 9;
 
     swapValues(a, b);
 
     cout << a << " " << b << endl;
 }
 
-// =============================
-// BAI 4
-// =============================
-void changeValue(int x) {
+// ======================================================
+// 4. REFERENCE VS VALUE
+// ======================================================
+void changeValue(int x)
+{
     x = 100;
 }
 
-void changeRef(int& x) {
+void changeRef(int& x)
+{
     x = 200;
 }
 
-void bai4() {
-    cout << "===== BAI 4 =====" << endl;
-
+void practice4()
+{
     int a = 5;
 
     changeValue(a);
@@ -75,20 +74,24 @@ void bai4() {
     cout << "After changeRef: " << a << endl;
 }
 
-// =============================
+// ======================================================
 // MAIN
-// =============================
-int main() {
-    bai1();
-    cout << endl;
+// ======================================================
+int main()
+{
+    cout << "===== PRACTICE 4 =====" << endl;
 
-    bai2();
-    cout << endl;
+    cout << "\n1. Modify through a Reference:" << endl;
+    practice1();
 
-    bai3();
-    cout << endl;
+    cout << "\n2. Use a Reference Parameter:" << endl;
+    practice2();
 
-    bai4();
+    cout << "\n3. Swap using References:" << endl;
+    practice3();
+
+    cout << "\n4. Reference vs Value:" << endl;
+    practice4();
 
     return 0;
 }
